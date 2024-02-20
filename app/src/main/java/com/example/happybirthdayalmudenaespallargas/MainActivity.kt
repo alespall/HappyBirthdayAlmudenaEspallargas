19826459fa805ffbef7da4759b4dd3b0d4c6c07d
alespall <alespall@unirioja.es>
2024-02-20 14:02:59 +0100

    compilar una app Simple p8

--- app/src/main/java/com/example/happybirthdayalmudenaespallargas/MainActivity.kt
+++ app/src/main/java/com/example/happybirthdayalmudenaespallargas/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.happybirthdayalmudenaespallargas
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -13,36 +15,40 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.example.happybirthdayalmudenaespallargas.ui.theme.HappyBirthdayAlmudenaEspallargasTheme
 
+@Composable
+fun GreetingText(message: String, from: String, modifier: Modifier=Modifier) {
+    Column(modifier=modifier) {
+        Text(
+            text = message,
+            fontSize =50.sp,
+            lineHeight = 56.sp,
+        )
+        Text(
+            text = from,
+            fontSize = 36.sp,
+        )
+    }
+}
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             HappyBirthdayAlmudenaEspallargasTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {}
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    GreetingText(message = "FELIZ CUMPLEAÑOS!", from = "De Almudena")
+                }
             }
         }
     }
-}
-
-@Composable
-fun GreetingText(message: String, from: String, modifier: Modifier=Modifier) {
-    Text(
-        text = message,
-        fontSize = 90.sp,
-        lineHeight = 100.sp,
-    )
-    Text(
-        text = from,
-        fontSize=36.sp,
-
-    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun BirthDayCardPreview() {
     HappyBirthdayAlmudenaEspallargasTheme {
-        GreetingText(message = "FELIZ CUMPLEAÑOS!", from="From Almudena")
+        GreetingText(message = "FELIZ CUMPLEAÑOS!", from="De Almudena")
     }
 }
+
