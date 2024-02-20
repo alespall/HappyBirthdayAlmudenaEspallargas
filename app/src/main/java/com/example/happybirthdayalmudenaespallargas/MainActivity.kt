9a65b21bc8b5ea255bd8a11ba4ef93c88edd68fa
alespall <alespall@unirioja.es>
2024-02-20 14:18:51 +0100

    crea un diseño basico, leccion 3 acabado

--- app/src/main/java/com/example/happybirthdayalmudenaespallargas/MainActivity.kt
+++ app/src/main/java/com/example/happybirthdayalmudenaespallargas/MainActivity.kt
@@ -3,33 +3,22 @@ package com.example.happybirthdayalmudenaespallargas
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.happybirthdayalmudenaespallargas.ui.theme.HappyBirthdayAlmudenaEspallargasTheme
-
-@Composable
-fun GreetingText(message: String, from: String, modifier: Modifier=Modifier) {
-    Column(modifier=modifier) {
-        Text(
-            text = message,
-            fontSize =50.sp,
-            lineHeight = 56.sp,
-        )
-        Text(
-            text = from,
-            fontSize = 36.sp,
-        )
-    }
-}
-
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,12 +26,37 @@ class MainActivity : ComponentActivity() {
             HappyBirthdayAlmudenaEspallargasTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    GreetingText(message = "FELIZ CUMPLEAÑOS!", from = "De Almudena")
+                    GreetingText(
+                        message = "FELIZ CUMPLEAÑOS!",
+                        from = "De Almudena",
+                        modifier = Modifier.padding(8.dp)
+                    )
                 }
             }
         }
     }
 }
+@Composable
+fun GreetingText(message: String, from: String, modifier: Modifier=Modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier=modifier
+    ) {
+        Text(
+            text = message,
+            fontSize =50.sp,
+            lineHeight = 56.sp,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = from,
+            fontSize = 36.sp,
+            modifier=Modifier.padding(16.dp).align(alignment = Alignment.End)
+        )
+    }
+}
+
+
 
 @Preview(showBackground = true)
 @Composable
