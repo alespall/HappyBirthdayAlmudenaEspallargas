3e584501be903d6a7d7a9fcc15f57fd8cbd517ec
alespall <alespall@unirioja.es>
2024-02-20 14:34:15 +0100

    crea un diseño basico, leccion 4.3

--- app/src/main/java/com/example/happybirthdayalmudenaespallargas/MainActivity.kt
+++ app/src/main/java/com/example/happybirthdayalmudenaespallargas/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.happybirthdayalmudenaespallargas
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -14,6 +15,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -56,13 +58,21 @@ fun GreetingText(message: String, from: String, modifier: Modifier=Modifier) {
     }
 }
 
+@Composable
+fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.androidparty)
+    Image(
+        painter = image,
+        contentDescription = null,
+    )
+}
 
 
 @Preview(showBackground = true)
 @Composable
 fun BirthDayCardPreview() {
     HappyBirthdayAlmudenaEspallargasTheme {
-        GreetingText(message = "FELIZ CUMPLEAÑOS!", from="De Almudena")
+        GreetingImage(message = "FELIZ CUMPLEAÑOS!", from="De Almudena")
     }
 }
 
