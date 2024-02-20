0fbbadad72f94ef81ef22fb60e1b7cdfd2cb579a
alespall <alespall@unirioja.es>
2024-02-20 14:46:45 +0100

    crea un diseño basico, leccion 4.5

--- app/src/main/java/com/example/happybirthdayalmudenaespallargas/MainActivity.kt
+++ app/src/main/java/com/example/happybirthdayalmudenaespallargas/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -15,6 +16,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -28,10 +30,9 @@ class MainActivity : ComponentActivity() {
             HappyBirthdayAlmudenaEspallargasTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    GreetingText(
+                    GreetingImage(
                         message = "FELIZ CUMPLEAÑOS!",
                         from = "De Almudena",
-                        modifier = Modifier.padding(8.dp)
                     )
                 }
             }
@@ -61,10 +62,21 @@ fun GreetingText(message: String, from: String, modifier: Modifier=Modifier) {
 @Composable
 fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) {
     val image = painterResource(R.drawable.androidparty)
-    Image(
-        painter = image,
-        contentDescription = null,
-    )
+    Box(modifier){
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            alpha = 0.5F,
+        )
+        GreetingText(
+            message = message,
+            from = from,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp)
+        )
+    }
 }
 
 
